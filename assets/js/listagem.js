f4a8b0075597c274416483439c696aaca1b39f41
SaulloRibeiro <saullorbarbosa2004santos@gmail.com>
2025-06-09 16:35:58 -0300

    Terminado o js do arquivo abrirAgenda.html

--- assets/js/listagem.js
+++ assets/js/listagem.js
@@ -3,22 +3,39 @@ const procedimentoComponente = document.getElementById("procedimentoId");
 const horarioComponente = document.getElementById("horarioId");
 const containerDaListagemProcedimento = document.getElementById("container-listagem-procedimentoId");
 
+let contador = 0;
 
 function addProcedimento(){
     let dataSelecionada = dataComponente.value;
     let procedimentoSelecionado = procedimentoComponente.value;
     let horarioSelecionado = horarioComponente.value;
 
-    let componente = `<div class="procedimento">
+    contador++;
+    
+    let componente = `<div class="procedimento" id="procedimento-${contador}">
                         <div class="dados">
                             <span>${procedimentoSelecionado}</span>
                             <span>${dataSelecionada}</span>
                             <span>${horarioSelecionado}</span>
                         </div>
                         <div class="btn-container">
-                            <button class="btn-remover">remover</button>
+                            <button type="button"class="btn-remover" onclick="deletar(${contador})">remover</button>
                         </div>
+                        <input type="hidden" name="procedimentos[]" value="${procedimentoSelecionado}">
+                        <input type="hidden" name="datas[]" value="${dataSelecionada}">
+                        <input type="hidden" name="horarios[]" value="${horarioSelecionado}">
                     </div>`;
     
-    containerDaListagemProcedimento.innerHTML += componente;
+
+    if(dataSelecionada !== "" && procedimentoSelecionado !== "" && horarioSelecionado !== ""){
+        containerDaListagemProcedimento.innerHTML += componente;
+        return;
+    }
+    alert("Preencha todos os campos, para poder adicionar o procedimento na agenda!");
+        
+}
+
+function deletar(id){
+    const elementoParaApagar = document.getElementById("procedimento-"+id);
+    elementoParaApagar.remove();
 }
