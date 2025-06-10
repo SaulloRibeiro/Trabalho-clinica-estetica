7fe76fe82d0ac3b334302dd60c56564d9c581cee
SaulloRibeiro <saullorbarbosa2004santos@gmail.com>
2025-06-09 21:48:42 -0300

    incluindo js no arquivo de login

--- assets/js/mascaras.js
+++ assets/js/mascaras.js
@@ -1,3 +1,19 @@
+const elementoSenha = document.getElementById("senhaId");
+
+
 function mascaraTelefone(input){
     input.value = input.value.replace(/^(\d{2})(\d{5})(\d{0,4})/, "($1) $2-$3");
+}
+
+function mostrarOuEsconderSenha(){
+    let iconeOlho = document.getElementById("olho");
+
+    if(elementoSenha.type === "password"){
+        iconeOlho.className = "fa-solid fa-eye";
+        elementoSenha.setAttribute("type", "text");
+    }
+    else{
+        iconeOlho.className = "fa-regular fa-eye-slash";
+        elementoSenha.setAttribute("type", "password");
+    }
 }
